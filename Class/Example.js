e74b170afcda46567e944dd09c730b2b0a0ccc60
unknown <sidhodhank2018@gmail.com>
2022-12-06 08:50:10 +0530

    [Sidhodhan]Added:Example Of Abstact and Interface

--- Class/Example.js
+++ Class/Example.js
@@ -14,89 +14,19 @@
 // console.log(result1);
 // let result2 = lion.eat("meat")
 // console.log(result2);
-// //example-2
-// class users {
-//     name = "";
-//     setName(name: string) {
-//         this.name = name;
-//     }
-//     display() {
-//         console.log(`${this.name}`);
-//     }
-// }
-// let u1 = new users
-// u1.setName("sid");
-// u1.display();
-// u1.name = "Sai"//Public varaible
-// u1.display();
-// //example-3
-// class users {
-//     private name = "";
-//     setName(name: string) {
-//         this.name = name;
-//     }
-//     display() {
-//         console.log(`${this.name}`);
-//     }
-// }
-// let u1 = new users
-// u1.setName("sid");
-// u1.display();
-// //u1.name = "Sai"//private varaible
-// u1.display();
-//example-4
-// //function overloading
-// function add(a: string, b: string): string;
-// function add(a: number, b: number): number;
-// function add(a: any, b: any): any {
-//     return a + b;
-// }
-// add("Sidhodhan ", "Kamble");
-// add(10, 20);
-// //example-5
-// //function overriding
-// class ParentClass {
-//     display(): void {
-//         console.log("Display method from Parent Class!")
-//     }
-// }
-// class ChildClass extends ParentClass {
-//     display(): void {
-//         super.display()
-//         console.log("dislay method from Child Class!")
-//     }
-// }
-// var parentObj = new ParentClass();
-// parentObj.display();
-// var childObj = new ChildClass();
-// childObj.display();
-// //example-6
-// //inheritance
-// class ParentClass {
-//     display(): void {
-//         console.log("Parent class")
-//     }
-// }
-// class ChildClass extends ParentClass {
-//     display(): void {
-//         super.display()
-//     }
-// }
-// var childObj = new ChildClass();
-// childObj.display();
-//example-7
-var Encapsulate = /** @class */ (function () {
-    function Encapsulate() {
-        this.str = "";
+var Name = /** @class */ (function () {
+    function Name(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-    Encapsulate.prototype.set = function (name) {
-        this.str = name;
+    Name.prototype.log = function () {
+        console.log("My Name is ".concat(this.firstName, " ").concat(this.lastName));
     };
-    Encapsulate.prototype.get = function () {
-        console.log("My name is ".concat(this.str));
+    Name.prototype.firstName1 = function () {
+        console.log("My First Name is ".concat(this.firstName));
     };
-    return Encapsulate;
+    return Name;
 }());
-var user = new Encapsulate();
-user.set("Sai");
-user.get();
+var Name1 = new Name('Sidhodhan', 'Kamble');
+Name1.log();
+Name1.firstName1();
--- Class/Example.ts
+++ Class/Example.ts
@@ -98,18 +98,94 @@
 // var childObj = new ChildClass();
 // childObj.display();
 
-//example-7
-class Encapsulate {
-    private str: string = ""
-    set(name: string) {
-        this.str = name;
+// //example-7
+// class Encapsulate {
+//     private str: string = ""
+//     set(name: string) {
+//         this.str = name;
 
-    }
-    get() {
-        console.log(`My name is ${this.str}`)
-    }
+//     }
+//     get() {
+//         console.log(`My name is ${this.str}`)
+//     }
+// }
+
+// var user = new Encapsulate();
+// user.set("Sai");
+// user.get();
+
+//example 7
+// //Abstration
+// abstract class Employee {
+
+//     name: string;
+//     paymentPerHour: number;
+
+//     constructor(name: string, paymentPerHour: number) {
+//         this.name = name;
+//         this.paymentPerHour = paymentPerHour;
+//     }
+
+//     public abstract calculateSalary(): number;
+// }
+
+// class Contractor extends Employee {
+
+//     workingHours: number;
+//     constructor(name: string, paymentPerHour: number, workingHours: number) {
+//         super(name, paymentPerHour);
+//         this.workingHours = workingHours;
+//     }
+
+//     calculateSalary(): number {
+//         return this.paymentPerHour * this.workingHours;
+//     }
+// }
+
+// class FullTimeEmployee extends Employee {
+//     constructor(name: string, paymentPerHour: number) {
+//         super(name, paymentPerHour);
+//     }
+
+//     calculateSalary(): number {
+//         return this.paymentPerHour * 8;
+//     }
+// }
+
+
+// let contractor: Employee;
+// let fullTimeEmployee: Employee;
+// contractor = new Contractor('Ramesh contractor', 10, 5);
+// fullTimeEmployee = new FullTimeEmployee('Ramesh full time employee', 8);
+
+// console.log(contractor.calculateSalary());
+// console.log(fullTimeEmployee.calculateSalary());
+
+// example-8
+//Interface
+interface firstName {
+    firstName: string;
+    firstName1(): void;
 }
 
-var user = new Encapsulate();
-user.set("Sai");
-user.get();
+interface lastName {
+    lastName: string;
+}
+
+class Name implements firstName, lastName {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    log() {
+        console.log(`My Name is ${this.firstName} ${this.lastName}`);
+    }
+    firstName1(): void {
+        console.log(`My First Name is ${this.firstName}`)
+    }
+}
+const Name1: Name = new Name('Sidhodhan', 'Kamble');
+Name1.log();
+Name1.firstName1();
